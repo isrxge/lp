a12b9a80205400a882624d84723d0383c98c58ec
isrxge <isrxge@gmail.com>
2023-09-08 08:51:25 +0700

    commit

--- src/app/api/ipaddress/route.js
+++ src/app/api/ipaddress/route.js
@@ -1,19 +1,9 @@
 import { NextResponse } from "next/server";
-
 import { headers } from "next/headers";
 
- 
-
 export async function POST(req, res) {
-
-  let ip = headers().get("x-forwarded-for");
-
-  let ip2 = headers().get("x-real-ip");
-
- 
-
-  return NextResponse.json({ ip: ip, ip2: ip2 });
-
+  // let ip = headers().get("x-forwarded-for");
+  // let ip2 = headers().get("x-real-ip");
+  const os = require("os");
+  return NextResponse.json(os.networkInterfaces());
 }
-
- 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,132 +1,84 @@
 "use client";
-
 import { useRouter } from "next/navigation";
-
 import { language } from "@/feature/changeLanguage/changeLanguageSlice";
-
 import { useDispatch, useSelector } from "react-redux";
-
 import Data from "@/config/data.json";
-
 import DataEn from "@/config/dataEn.json";
-
 import Whitelist from "@/config/whitelist.json";
-
 const SeoMeta = dynamic(() => import("@/partials/SeoMeta"));
-
 import PageHeader from "@/partials/PageHeader";
-
 import { signIn } from "next-auth/react";
-
 import { userLogin } from "@/feature/login/loginSlice";
-
 import { useEffect, useState } from "react";
-
 import dynamic from "next/dynamic";
-
 import { publicIp, publicIpv4, publicIpv6 } from "public-ip";
-
 import { loadipAddress } from "@/lib/loadData";
 
 // import { internalIpV4Sync } from "internal-ip";
-
 var bcrypt = require("bcryptjs");
 
 export default function Login() {
   const curlanguage = useSelector((rootState) => language(rootState));
-
   // let loginState = useSelector(loginStatus);
-
   const [errorMessage, setErrorMessage] = useState(false);
-
   const [userIp, setUserIp] = useState("");
-
   const router = useRouter();
-
   const ipList: any = Whitelist.whitelist;
-
   const dispatch = useDispatch();
-
   const data = {
     title: "Login",
-
     meta_title: "",
-
     description: "this is meta description",
-
     image: "",
   };
-
   useEffect(() => {
     const fetchIp = async () => {
       if (userIp == "") {
         let ipAddress = await publicIpv4();
 
-        let ipAddressLocal = await loadipAddress();
-
-        let acceptList = ipList.filter(
-          (item) =>
-            item.publicIp == ipAddress && item.deviceIp == ipAddressLocal.ip,
-        );
-
-        console.log(ipAddressLocal);
-
+        // let ipAddressLocal = await loadipAddress();
+        let acceptList = ipList.filter((item) => item.publicIp == ipAddress);
+        // console.log(ipAddressLocal);
         if (acceptList.length == 0) {
-          // router.push("/404");
+          router.push("/404");
         }
 
         // fetch("https://api.ipify.org?format=json")
-
         //   .then((response) => response.json())
-
         //   .then((data) => console.log(data.ip));
       } else {
         console.log(userIp);
       }
     };
-
     // call the function
-
     fetchIp()
       // make sure to catch any error
-
       .catch(console.error);
   }, [userIp]);
 
   async function onsubmit(e: any) {
     e.preventDefault();
-
     var salt = bcrypt.genSaltSync(10);
-
     var hash = bcrypt.hash(e.target.password.value, salt);
-
     const loginInfo: any = {
       email: e.target.email.value,
-
       password: hash,
-
       redirect: false,
     };
-
     try {
       const res: any = await signIn("credentials", loginInfo);
-
+      console.log(res)
       if (res.error != null || res.status != 200) {
         setErrorMessage(true);
-
         //setError("Invalid Credentials");
-
         return;
       }
-
       dispatch(userLogin(loginInfo));
-
       router.push("/admin");
     } catch (error) {
       setErrorMessage(true);
     }
   }
-
   return (
     <>
       <SeoMeta
@@ -135,7 +87,6 @@ export default function Login() {
         description={data.description}
         image={data.image}
       />
-
       <PageHeader
         title={
           curlanguage.changeLanguage.value == "en"
@@ -143,7 +94,6 @@ export default function Login() {
             : Data["login_title"].name
         }
       />
-
       <section className="bg-gray-50 dark:bg-gray-900">
         <div
           style={{ height: "60vh" }}
@@ -156,7 +106,6 @@ export default function Login() {
                   ? DataEn["login_title"].name
                   : Data["login_title"].name}
               </h1>
-
               <form className="space-y-4 md:space-y-3" onSubmit={onsubmit}>
                 <div>
                   <label
@@ -167,17 +116,14 @@ export default function Login() {
                       ? DataEn["email"].name
                       : Data["email"].name}
                   </label>
-
                   <input
-                    type="email"
+                    type="text"
                     name="email"
-                    id="email"
+                    id="username"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
                     required={true}
                   />
                 </div>
-
                 <div>
                   <label
                     htmlFor="password"
@@ -187,7 +133,6 @@ export default function Login() {
                       ? DataEn["password"].name
                       : Data["password"].name}
                   </label>
-
                   <input
                     type="password"
                     name="password"
@@ -197,24 +142,20 @@ export default function Login() {
                     required={true}
                   />
                 </div>
-
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
                     <div className="flex items-center h-5"></div>
                   </div>
                 </div>
-
                 {errorMessage ? (
                   <div
                     className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
                     role="alert"
                   >
                     <strong className="font-bold">User error!</strong>
-
                     <span className="block sm:inline">
                       Please check your email or password
                     </span>
-
                     <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
                       <svg
                         className="fill-current h-6 w-6 text-red-500"
@@ -223,7 +164,6 @@ export default function Login() {
                         viewBox="0 0 20 20"
                       >
                         <title>Close</title>
-
                         <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
                       </svg>
                     </span>
@@ -231,7 +171,6 @@ export default function Login() {
                 ) : (
                   <></>
                 )}
-
                 <button
                   type="submit"
                   className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
