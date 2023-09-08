3181e13789ba10b4748cec894dfa5b2672f4e390
isrxge <isrxge@gmail.com>
2023-09-08 08:56:18 +0700

    commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,9 +36,11 @@ export default function Login() {
       if (userIp == "") {
         let ipAddress = await publicIpv4();
 
-        // let ipAddressLocal = await loadipAddress();
+        let ipAddressLocal = await loadipAddress();
+
         let acceptList = ipList.filter((item) => item.publicIp == ipAddress);
-        // console.log(ipAddressLocal);
+
+        console.log(ipAddressLocal);
         if (acceptList.length == 0) {
           router.push("/404");
         }
@@ -67,7 +69,7 @@ export default function Login() {
     };
     try {
       const res: any = await signIn("credentials", loginInfo);
-      console.log(res)
+      console.log(res);
       if (res.error != null || res.status != 200) {
         setErrorMessage(true);
         //setError("Invalid Credentials");
