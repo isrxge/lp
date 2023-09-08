dc6ce02ee3b29ffb360162ab71a4f0fd97ac3526
isrxge <isrxge@gmail.com>
2023-09-08 09:54:35 +0700

    commit

--- src/app/api/ipaddress/route.js
+++ src/app/api/ipaddress/route.js
@@ -1,9 +1,17 @@
 import { NextResponse } from "next/server";
 
+ 
+
 import arp from "@network-utils/arp-lookup";
+
 export async function POST(req, res) {
+
   // let ip = headers().get("x-forwarded-for");
+
   // let ip2 = headers().get("x-real-ip");
+
   let results = await arp.getTable();
-  return NextResponse.json(results);
+
+  return NextResponse.json({ results: results });
+
 }
