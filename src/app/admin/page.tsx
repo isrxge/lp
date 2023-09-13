1d67ab67e3320b12d0ff98dc38430aa4aaf302a9
isrxge <isrxge@gmail.com>
2023-09-13 15:53:53 +0700

    commit

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -46,44 +46,90 @@ import { useEffect, useState } from "react";
 import { Button } from "@mantine/core";
 import IpTable from "@/components/admin/ip/ipTable";
 import AddIp from "@/components/admin/ip/addIp";
+import { updateUserLoginCount } from "@/lib/updateData";
+import ToastGenerator from "@/lib/toast-tify";
 
-// import "../styles/homepage.scss"
+//admin page
 const Home = () => {
+  //show banner table
   const [showBanner, setShowBanner] = useState("visible");
+  //show contact table
   const [showContact, setShowContact] = useState("hidden");
+  //show customer table
   const [showCustomer, setShowCustomer] = useState("hidden");
+  //show article(New) table
   const [showNew, setShowNew] = useState("hidden");
+  //show message table
   const [showMessage, setShowMessage] = useState("hidden");
+  //show partner table
   const [showPartner, setShowPartner] = useState("hidden");
+  //show product table
   const [showProduct, setShowProduct] = useState("hidden");
+  //show user table
   const [showUser, setShowUser] = useState("hidden");
+  //show change password page
   const [showChangePassword, setShowChangePassword] = useState("hidden");
+  //is call api success
+  const [isSucess, setIsSucess] = useState(false);
+  //set show message
+  const [sucessMessage, setSucessMessage] = useState("");
+  //check user login status
   const { status }: any = useSession({
     required: true,
     onUnauthenticated() {
       redirect("/login");
     },
   });
+  //check user login session
   const { data: session }: any = useSession();
   useEffect(() => {
-    if (status !== "loading") {
-      if (session.user.loginCount == 0) {
-        // redirect("/admin/changepassword");
-        // setShowChangePassword("visible");
-        // setShowBanner("hidden");
-        // setShowContact("hidden");
-        // setShowCustomer("hidden");
-        // setShowNew("hidden");
-        // setShowMessage("hidden");
-        // setShowPartner("hidden");
-        // setShowProduct("hidden");
-        // setShowUser("hidden");
+    //check user is first time login or not
+    const checkuserStatus = async () => {
+      if (status !== "loading") {
+        if (session.user.loginCount == 0) {
+          //is first time login will direct user to change password
+
+          setShowChangePassword("visible");
+          setShowBanner("hidden");
+          setShowContact("hidden");
+          setShowCustomer("hidden");
+          setShowNew("hidden");
+          setShowMessage("hidden");
+          setShowPartner("hidden");
+          setShowProduct("hidden");
+          setShowUser("hidden");
+        } else {
+          //user already login more than once
+          let userData = {
+            _id: session.user._id,
+            loginCount: session.user.loginCount + 1,
+          };
+          let returnResult = await updateUserLoginCount(userData, session); //will update user login count
+          if (returnResult.success != undefined) {
+            showToast(returnResult.msg);
+          }
+        }
+      } else {
       }
-    }
-  }, [status, session]);
+    };
+    // call the function
+    checkuserStatus()
+      // make sure to catch any error
+      .catch(console.error);
+  }, [status]);
+  const showToast = (msg) => {
+    //set a toast message in 10 second
+    setIsSucess(true);
+    setSucessMessage(msg);
+    setTimeout(() => {
+      setIsSucess(false);
+      setSucessMessage("");
+    }, 10000);
+  };
   useEffect(() => {
-    onInactive(600000, function () {
-      //signOut();
+    //sign out after 1 hour of not interacting
+    onInactive(3600000, function () {
+      signOut({ callbackUrl: "http://lp.com.vn/login" });
     });
     function onInactive(ms, cb) {
       var wait = setTimeout(cb, ms);
@@ -101,12 +147,14 @@ const Home = () => {
     }
   }, []);
   const logOut = () => {
+    //logout action
     signOut();
-    // redirect("/login");
   };
-  const onClick = (tabNo: string) => {
+  const onClick = (tabNo: string, e) => {
+    e.preventDefault();
+    //change tab
     switch (tabNo) {
-      case "banner":
+      case "banner": //banner tab
         setShowBanner("visible");
         setShowContact("hidden");
         setShowCustomer("hidden");
@@ -116,7 +164,7 @@ const Home = () => {
         setShowProduct("hidden");
         setShowUser("hidden");
         break;
-      case "contact":
+      case "contact": //contact tab
         setShowBanner("hidden");
         setShowContact("visible");
         setShowCustomer("hidden");
@@ -126,7 +174,7 @@ const Home = () => {
         setShowProduct("hidden");
         setShowUser("hidden");
         break;
-      case "customer":
+      case "customer": //customer tab
         setShowBanner("hidden");
         setShowContact("hidden");
         setShowCustomer("visible");
@@ -136,7 +184,7 @@ const Home = () => {
         setShowProduct("hidden");
         setShowUser("hidden");
         break;
-      case "new":
+      case "new": //new tab
         setShowBanner("hidden");
         setShowContact("hidden");
         setShowCustomer("hidden");
@@ -146,7 +194,7 @@ const Home = () => {
         setShowProduct("hidden");
         setShowUser("hidden");
         break;
-      case "message":
+      case "message": //message tab
         setShowBanner("hidden");
         setShowContact("hidden");
         setShowCustomer("hidden");
@@ -156,7 +204,7 @@ const Home = () => {
         setShowProduct("hidden");
         setShowUser("hidden");
         break;
-      case "partner":
+      case "partner": //partner tab
         setShowBanner("hidden");
         setShowContact("hidden");
         setShowCustomer("hidden");
@@ -166,7 +214,7 @@ const Home = () => {
         setShowProduct("hidden");
         setShowUser("hidden");
         break;
-      case "product":
+      case "product": //product tab
         setShowBanner("hidden");
         setShowContact("hidden");
         setShowCustomer("hidden");
@@ -176,7 +224,7 @@ const Home = () => {
         setShowProduct("visible");
         setShowUser("hidden");
         break;
-      case "user":
+      case "user": //user tab
         setShowBanner("hidden");
         setShowContact("hidden");
         setShowCustomer("hidden");
@@ -189,15 +237,8 @@ const Home = () => {
     }
   };
   const handleSaveClick = () => {
-    setShowChangePassword("hidden");
-    setShowBanner("visible");
-    setShowContact("hidden");
-    setShowCustomer("hidden");
-    setShowNew("hidden");
-    setShowMessage("hidden");
-    setShowPartner("hidden");
-    setShowProduct("hidden");
-    setShowUser("hidden");
+    //signout when change password completed
+    signOut({ callbackUrl: "http://lp.com.vn/login" });
   };
   if (status == "loading") {
     return <></>;
@@ -208,6 +249,7 @@ const Home = () => {
       </div>
     ) : (
       <div className="max-w-7xl mx-auto ">
+        {isSucess ? <ToastGenerator message={sucessMessage} /> : <></>}
         <Button onClick={() => logOut()} variant="default">
           Log out
         </Button>
@@ -220,98 +262,112 @@ const Home = () => {
           >
             <li className="mr-2" role="presentation">
               <button
-                className="inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                className={`${
+                  showBanner == "visible" ? "text-gray-900" : "text-gray-500 "
+                } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                 id="banner-tab"
                 data-tabs-target="#banner"
                 type="button"
                 role="tab"
                 aria-controls="banner"
                 aria-selected="true"
-                onClick={() => onClick("banner")}
+                onClick={(e) => onClick("banner", e)}
               >
                 Banner
               </button>
             </li>
             <li className="mr-2" role="presentation">
               <button
-                className="inline-block text-gray-500 active:text-gray-600 active:border-gray-300 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                className={`${
+                  showContact == "visible" ? "text-gray-900" : "text-gray-500 "
+                } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                 id="company-tab"
                 data-tabs-target="#company"
                 type="button"
                 role="tab"
                 aria-controls="company"
                 aria-selected="false"
-                onClick={() => onClick("contact")}
+                onClick={(e) => onClick("contact", e)}
               >
                 Company
               </button>
             </li>
             <li className="mr-2" role="presentation">
               <button
-                className="inline-block text-gray-500 active:text-gray-600 active:border-gray-300 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                className={`${
+                  showCustomer == "visible" ? "text-gray-900" : "text-gray-500 "
+                } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                 id="customer-tab"
                 data-tabs-target="#customer"
                 type="button"
                 role="tab"
                 aria-controls="customer"
                 aria-selected="false"
-                onClick={() => onClick("customer")}
+                onClick={(e) => onClick("customer", e)}
               >
                 Customer
               </button>
             </li>
             <li role="presentation">
               <button
-                className="inline-block text-gray-500 active:text-gray-600 active:border-gray-300 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                className={`${
+                  showNew == "visible" ? "text-gray-900" : "text-gray-500 "
+                } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                 id="new-tab"
                 data-tabs-target="#new"
                 type="button"
                 role="tab"
                 aria-controls="new"
                 aria-selected="false"
-                onClick={() => onClick("new")}
+                onClick={(e) => onClick("new", e)}
               >
                 News
               </button>
             </li>
             <li role="presentation">
               <button
-                className="inline-block text-gray-500 active:text-gray-600 active:border-gray-300 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                className={`${
+                  showMessage == "visible" ? "text-gray-900" : "text-gray-500 "
+                } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                 id="message-tab"
                 data-tabs-target="#message"
                 type="button"
                 role="tab"
                 aria-controls="message"
                 aria-selected="false"
-                onClick={() => onClick("message")}
+                onClick={(e) => onClick("message", e)}
               >
                 Messages
               </button>
             </li>
             <li role="presentation">
               <button
-                className="inline-block text-gray-500 active:text-gray-600 active:border-gray-300 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                className={`${
+                  showPartner == "visible" ? "text-gray-900" : "text-gray-500 "
+                } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                 id="partner-tab"
                 data-tabs-target="#partner"
                 type="button"
                 role="tab"
                 aria-controls="partner"
                 aria-selected="false"
-                onClick={() => onClick("partner")}
+                onClick={(e) => onClick("partner", e)}
               >
                 Partner
               </button>
             </li>
             <li role="presentation">
               <button
-                className="inline-block text-gray-500 active:text-gray-600 active:border-gray-300 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                className={`${
+                  showProduct == "visible" ? "text-gray-900" : "text-gray-500 "
+                } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                 id="product-tab"
                 data-tabs-target="#product"
                 type="button"
                 role="tab"
                 aria-controls="product"
                 aria-selected="false"
-                onClick={() => onClick("product")}
+                onClick={(e) => onClick("product", e)}
               >
                 Product
               </button>
@@ -319,14 +375,16 @@ const Home = () => {
             {status != "loading" && session.user.role == "sysadmin" ? (
               <li role="presentation">
                 <button
-                  className="inline-block text-gray-500 active:text-gray-600 active:border-gray-300 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                  className={`${
+                    showUser == "visible" ? "text-gray-900" : "text-gray-500 "
+                  } inline-block active:text-gray-600 active:border-gray-300 text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 text-sm font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300`}
                   id="contacts-tab"
                   data-tabs-target="#contacts"
                   type="button"
                   role="tab"
                   aria-controls="contacts"
                   aria-selected="false"
-                  onClick={() => onClick("user")}
+                  onClick={(e) => onClick("user", e)}
                 >
                   User
                 </button>
--- src/layouts/components/admin/contact/tableContact.tsx
+++ src/layouts/components/admin/contact/tableContact.tsx
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from "react";
 
-import { Table, Modal, Button, Box, Grid, Col, TextInput } from "@mantine/core"; // Import thêm Button
+import {
+  Table,
+  Modal,
+  Button,
+  Box,
+  Grid,
+  Col,
+  TextInput,
+  Textarea,
+  Group,
+  Switch,
+  ActionIcon,
+} from "@mantine/core"; // Import thêm Button
 import { loadContact } from "@/lib/loadData";
 import { useUrl } from "nextjs-current-url";
 import { useSession } from "next-auth/react";
 import { updateContactInfo } from "@/lib/updateData";
 import ToastGenerator from "@/lib/toast-tify";
+import { useForm } from "@mantine/form";
 
 interface Contact {
   _id: string;
@@ -31,8 +44,6 @@ function ContactTable() {
   let { data: session, status }: any = useSession();
   const [selectedContact, setSelectedContact] = useState<Contact | null>(null);
 
-  const [addContactVisible, setAddContactVisible] = useState(false);
-
   const [isEditMode, setIsEditMode] = useState(false);
 
   const [showContent, setShowContent] = useState(false);
@@ -51,23 +62,16 @@ function ContactTable() {
       .catch(console.error);
   }, [contactData]);
 
-  // const handleEditClick = (news: News) => {
-
-  //   setSelectedNews(news);
-
-  //   setEditNewsVisible(true);
-
-  // };
-
   const handleEditClick = (contact: Contact) => {
     setSelectedContact(contact);
 
     setIsEditMode(selectedContact === contact); // Chỉ thiết lập isEditMode thành true nếu sản phẩm đã được chọn đang được chỉnh sửa
+    form.setValues(contact);
   };
 
-  const handleSaveClick = async () => {
+  const handleSaveClick = async (values) => {
     // Thực hiện lưu thay đổi vào cơ sở dữ liệu (gọi API, ...)
-    let returnResult = await updateContactInfo(selectedContact, session);
+    let returnResult = await updateContactInfo(values, session);
     if (returnResult.success != undefined) {
       showToast(returnResult.msg);
     }
@@ -81,16 +85,9 @@ function ContactTable() {
       setSucessMessage("");
     }, 10000);
   };
-  const handleViewClick = (news: Contact) => {
-    setSelectedContact(news);
-
-    setIsEditMode(false); // Chuyển sang chế độ xem
-  };
-
-  const toggleShowContent = () => {
-    setShowContent(!showContent);
-  };
-
+  const form = useForm({
+    initialValues: JSON.parse(JSON.stringify({})),
+  });
   const rows = contactData.map((contact, index) => (
     <tr key={contact._id}>
       <td>{index + 1}</td>
@@ -102,7 +99,6 @@ function ContactTable() {
       <td>{contact.linkWebsite}</td>
 
       <td>
-        <button onClick={() => setSelectedContact(contact)}>View</button> |{" "}
         <button onClick={() => handleEditClick(contact)}>Edit</button>
       </td>
 
@@ -125,7 +121,7 @@ function ContactTable() {
         Add News
 
       </Button> */}
-      {isSucess ? <ToastGenerator message={sucessMessage} /> : <></>}
+
       <Table>
         <thead>
           <tr>
@@ -159,16 +155,17 @@ function ContactTable() {
               <h3 className="flex justify-center">
                 {isEditMode ? "Edit Contact" : "Contact Details"}
               </h3>
-
-              <Box maw={800} mx="auto">
-                <form>
+              {isSucess ? <ToastGenerator message={sucessMessage} /> : <></>}
+              <Box maw={900} mx="auto">
+                <form
+                  onSubmit={form.onSubmit((values) => handleSaveClick(values))}
+                >
                   <Grid gutter="lg">
                     <Col span={6}>
                       <label>Address: </label>
 
-                      <textarea
-                        value={selectedContact.address}
-                        disabled
+                      <Textarea
+                        {...form.getInputProps("address")}
                         style={{ width: "100%", height: "90px" }}
                       />
                     </Col>
@@ -176,10 +173,9 @@ function ContactTable() {
                     <Col span={6}>
                       <label>Address (English): </label>
 
-                      <textarea
-                        value={selectedContact.addressEn}
-                        disabled
+                      <Textarea
                         style={{ width: "100%", height: "90px" }}
+                        {...form.getInputProps("addressEn")}
                       />
                     </Col>
 
@@ -187,9 +183,8 @@ function ContactTable() {
                       <label>Address Link: </label>
 
                       <TextInput
-                        value={selectedContact.addressLink}
-                        disabled
                         style={{ width: "100%", height: "90px" }}
+                        {...form.getInputProps("addressLink")}
                       />
                     </Col>
 
@@ -197,8 +192,7 @@ function ContactTable() {
                       <label>Link Website: </label>
 
                       <TextInput
-                        value={selectedContact.linkWebsite}
-                        disabled
+                        {...form.getInputProps("linkWebsite")}
                         style={{ width: "100%", height: "90px" }}
                       />
                     </Col>
@@ -206,9 +200,8 @@ function ContactTable() {
                     <Col span={4}>
                       <label>Email: </label>
 
-                      <input
-                        value={selectedContact.email}
-                        disabled
+                      <TextInput
+                        {...form.getInputProps("email")}
                         style={{ width: "100%", height: "90px" }}
                       />
                     </Col>
@@ -216,59 +209,82 @@ function ContactTable() {
                     <Col span={4}>
                       <label>Phone Number: </label>
 
-                      <textarea
-                        value={selectedContact.phoneNumber.join("\n")} // Join các phần tử cách nhau bằng dấu xuống dòng
-                        disabled
-                        style={{ width: "100%", height: "90px" }} // Đặt chiều rộng và chiều cao
-                      />
+                      {selectedContact.phoneNumber.map((number, index) => (
+                        <Group key={number} mt="xs">
+                          <TextInput
+                            {...form.getInputProps(`phoneNumber.${index}`)}
+                            style={{ width: "70%", height: "50px" }}
+                          />
+                          <ActionIcon
+                            color="red"
+                            onClick={() =>
+                              form.removeListItem("phoneNumber", index)
+                            }
+                          >
+                            remove
+                          </ActionIcon>
+                        </Group>
+                      ))}
+                      {/* <Group position="center" mt="md">
+                        <Button
+                          variant="default"
+                          onClick={() => form.insertListItem("phoneNumber", "")}
+                        >
+                          Add phone number
+                        </Button>
+                      </Group> */}
                     </Col>
 
                     <Col span={4}>
                       <label>Social Account: </label>
-
-                      <textarea
-                        value={selectedContact.socialAccount.join("\n")} // Join các phần tử cách nhau bằng dấu xuống dòng
-                        disabled
-                        style={{ width: "100%", height: "90px" }} // Đặt chiều rộng và chiều cao
-                      />
+                      {selectedContact.socialAccount.map((account, index) => (
+                        <Group key={account} mt="xl">
+                          <Textarea
+                            {...form.getInputProps(`socialAccount.${index}`)}
+                            style={{ width: "70%", height: "150px" }}
+                          />
+                          <ActionIcon
+                            color="red"
+                            onClick={() =>
+                              form.removeListItem("socialAccount", index)
+                            }
+                          >
+                            remove
+                          </ActionIcon>
+                        </Group>
+                      ))}
+                      {/* <Group position="center" mt="md">
+                        <Group position="center" mt="md">
+                          <Button
+                            variant="default"
+                            onClick={() =>
+                              form.insertListItem("socialAccount", "")
+                            }
+                          >
+                            Add account
+                          </Button>
+                        </Group>
+                      </Group> */}
                     </Col>
+                    <Col span={6} className="flex justify-end mt-6">
+                      {/* Thêm class CSS để đặt nút submit ở góc phải */}
 
+                      <Button
+                        type="submit"
+                        style={{ backgroundColor: "blue" }}
+                        size="md"
+                      >
+                        Submit
+                      </Button>
+                    </Col>
                     {/* ... Các phần khác tương tự như trên */}
                   </Grid>
                 </form>
               </Box>
-
-              <div style={{ marginTop: "16px" }}>
-                {isEditMode ? (
-                  <Button
-                    type="button"
-                    onClick={handleSaveClick}
-                    style={{ backgroundColor: "#28a745", color: "white" }}
-                  >
-                    Save
-                  </Button>
-                ) : (
-                  <Button
-                    type="button"
-                    onClick={() => setIsEditMode(true)}
-                    style={{ backgroundColor: "#007bff", color: "white" }}
-                  >
-                    Edit
-                  </Button>
-                )}
-              </div>
             </div>
           </section>
         )}
       </Modal>
-
-      {/* <Modal
-        size="1000px"
-        opened={addContactVisible}
-        onClose={() => setAddContactVisible(false)}
-      >
-        <ContactForm onClose={() => setAddContactVisible(false)} />
-      </Modal> */}
     </div>
   );
 }
